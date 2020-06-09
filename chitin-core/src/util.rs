5b1b13238b1d7c61351cf014a5d9319671f2f011
davidthyu <davidth.yu@moxa.com>
2020-06-09 09:28:10 +0800

    可以生成 query 枚舉的 json 形式了！

--- chitin-core/src/util.rs
+++ chitin-core/src/util.rs
@@ -1,4 +1,14 @@
 use crate::Request;
 pub fn gen_enum_json(prev: &[&'static str], params: &[Request]) -> String {
-    "{}".to_owned()
+    if let Some(cur) = prev.last() {
+        let inner = gen_enum_json(&prev[0..prev.len() - 1], params);
+        format!("{{ \"{}\": {} }}", cur, inner)
+    } else {
+        let inner = params
+            .iter()
+            .map(|r| r.name.clone())
+            .collect::<Vec<_>>()
+            .join(", ");
+        format!("{{ {} }}", inner)
+    }
 }
